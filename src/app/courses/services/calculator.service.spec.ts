7dd745f0d4682f32e64d02d715e9183104d4dc3a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-04 11:38:27 -0500

    Refactor CalculatorService test, make it DRY

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,19 +1,23 @@
 import { CalculatorService } from "./calculator.service";
 
 describe("CalculatorService", () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerSpyService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  })
+
   it("should add two numbers", () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
     const result = calculator.add(2, 2);
     expect(result).toBe(4, 'unexpected add result');
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
