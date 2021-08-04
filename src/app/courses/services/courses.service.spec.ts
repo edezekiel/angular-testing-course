07c4b46a279560ac74b6c23eb61240e78a4aeb92
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-04 13:21:09 -0500

    Use afterEach to trigger httpTestingController.verify()

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,7 +4,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
-import { COURSES } from '../../../../server/db-data';
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService;
@@ -15,22 +15,36 @@ describe("CoursesService", () => {
       providers: [CoursesService],
       imports: [HttpClientTestingModule],
     });
-  
+
     coursesService = TestBed.inject(CoursesService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it("should retrieve all courses,", () => {
-    coursesService.findAllCourses()
-      .subscribe(courses => {
-        expect(courses).toBeTruthy('No courses returned');
-        expect(courses.length).toBe(12, 'incorrect number of courses');
-        const course = courses.find(course => course.id === 12);
-        expect(course.titles.description).toBe('Angular Testing Course');
-      });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
-      const req = httpTestingController.expectOne('/api/courses');
-      expect(req.request.method).toEqual('GET');
-      req.flush({ payload: Object.values(COURSES) })
+  it("should retrieve all courses,", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses returned");
+      expect(courses.length).toBe(12, "incorrect number of courses");
+      const course = courses.find((course) => course.id === 12);
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = httpTestingController.expectOne("/api/courses");
+    expect(req.request.method).toEqual("GET");
+    req.flush({ payload: Object.values(COURSES) });
+  });
+
+  it("should find a course by id", () => {
+    coursesService.findCourseById(12).subscribe((course) => {
+      expect(course).toBeTruthy();
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("GET");
+    req.flush(COURSES[12]);
   });
 });
