ec2894b72f8491b6b6f05f435cfe087c9f6229f5
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-23 11:08:15 -0500

    Explore differences between promises and async methods in the queue

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,28 +1,55 @@
-import { fakeAsync, flush, tick } from '@angular/core/testing';
+import { fakeAsync, flush, tick } from "@angular/core/testing";
 
-describe('Async Testing Examples', () => {
-  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
+describe("Async Testing Examples", () => {
+  it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
-      console.log('running Async Testing Examples assertions');
+      console.log("running Async Testing Examples assertions");
       test = true;
       expect(test).toBeTruthy();
       done();
     }, 1000);
   });
 
-  it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+  it("Asynchronous test example - setTimeout()", fakeAsync(() => {
     let test = false;
 
     setTimeout(() => {});
     setTimeout(() => {
-      console.log('running assertions');
+      console.log("running assertions");
       test = true;
     }, 1000);
 
     // tick(1000);
     flush();
-    
+
     expect(test).toBeTruthy();
   }));
+
+  fit("Asynchronous test example - plain Promise", () => {
+    let test = false;
+
+    console.log("Creating promise");
+
+    setTimeout(() => {
+      console.log("setTimeout() 1st callback triggered");
+      test = true;
+    });
+
+    setTimeout(() => {
+      console.log("setTimeout() 2nd callback triggered");
+      test = true;
+    });
+
+    Promise.resolve().then(() => {
+      console.log("Promise first then() evaluated successfully");
+      return Promise.resolve();
+    }).then(() => {
+      console.log("Promise second then() evaluated successfully");
+      test = true;
+    })
+
+    console.log("Running test assertions, test = ", test);
+    expect(test).toBeTruthy();
+  });
 });
