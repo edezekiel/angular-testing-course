8dec1b9a1ac1c402be73f09f01ea0f634905fd27
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-23 10:14:42 -0500

    Create async test examples using fakeAsync

---
+++ src/app/courses/home/async-examples.spec.ts
@@ -0,0 +1,25 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+
+describe('Async Testing Examples', () => {
+  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
+    let test = false;
+    setTimeout(() => {
+      console.log('running Async Testing Examples assertions');
+      test = true;
+      expect(test).toBeTruthy();
+      done();
+    }, 1000);
+  });
+
+  it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {
+      console.log('running assertions');
+      test = true;
+      expect(test).toBeTruthy();
+    }, 1000);
+
+    tick(1000);
+  }));
+});
