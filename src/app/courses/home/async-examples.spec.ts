8d8f2677cead35b100f746ca3b0b2bf6195b7993
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-23 11:45:39 -0500

    Use fakeAsync to test observables

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,10 +1,11 @@
 import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 describe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
-      console.log("running Async Testing Examples assertions");
       test = true;
       expect(test).toBeTruthy();
       done();
@@ -16,11 +17,9 @@ describe("Async Testing Examples", () => {
 
     setTimeout(() => {});
     setTimeout(() => {
-      console.log("running assertions");
       test = true;
     }, 1000);
 
-    // tick(1000);
     flush();
 
     expect(test).toBeTruthy();
@@ -29,23 +28,18 @@ describe("Async Testing Examples", () => {
   it("Asynchronous test example - plain Promise", fakeAsync(() => {
     let test = false;
 
-    console.log("Creating promise");
-
-    Promise.resolve().then(() => {
-      console.log("Promise first then() evaluated successfully");
-      test = true;
-      return Promise.resolve();
-    }).then(() => {
-      console.log("Promise second then() evaluated successfully");
-    })
-
-    console.log("Running test assertions, test = ", test);
+    Promise.resolve()
+      .then(() => {
+        test = true;
+        return Promise.resolve();
+      })
+      .then(() => {});
 
     flushMicrotasks();
     expect(test).toBeTruthy();
   }));
 
-  it('Asynchronous test example -  Promises + setTimeout()', fakeAsync(() => {
+  it("Asynchronous test example -  Promises + setTimeout()", fakeAsync(() => {
     let counter = 0;
 
     Promise.resolve().then(() => {
@@ -62,5 +56,15 @@ describe("Async Testing Examples", () => {
     expect(counter).toBe(10);
     tick(500);
     expect(counter).toBe(11);
+  }));
+
+  it("Asynchronous test example - Observables", fakeAsync(() => {
+    let test = false;
+    const test$ = of(test).pipe(delay(1000));
+    test$.subscribe(() => {
+      test = true;
+    });
+    tick(1000);
+    expect(test).toBe(true);
   }));
 });
